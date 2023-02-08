e08974ed9ebec1ea3e62f9d9f16a5e11314be835
Andres Alba <tensinkusum@example.com>
2023-02-08 00:28:58 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
+
+  const [resourceType, setResourceType] = useState('posts');
+  const [items, setItems] = useState([]);
+
+  const [windowWith, setWindowWith] = useState(window.innerWidth);
+
+  console.log('rendered')
+
+  useEffect(() => {
+    console.log('useEffect')
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
+  }, [resourceType])//only fetches when resourceType changes
+
+  const handleResize = () => {
+    setWindowWith(window.innerWidth)
+  }
+
+  useEffect(() => {
+    console.log('useEffect window')
+    window.addEventListener('resize', handleResize)
+    return () => {//this return is the cleaning or unmount
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])//On mount capture the size of the browser window
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Window Width: {windowWith}</h1>
+      <div className="App">
+        <button onClick={() => { setResourceType('posts') }} >Posts</button>
+        <button onClick={() => { setResourceType('users') }} >Users</button>
+        <button onClick={() => { setResourceType('comments') }} >Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+      {items.map(item => {
+        return <pre>{JSON.stringify(item)}</pre>
+      })}
+
+    </>
   );
 }
 
